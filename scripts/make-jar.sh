b825b3fa87105ffd24b6f53f0d2680948132c03b
Josh Green <josh@softwareverde.com>
2017-06-07 22:30:07 -0400

    Updated make script.

--- scripts/make-jar.sh
+++ scripts/make-jar.sh
@@ -3,7 +3,5 @@
 rm -rf out/bin 2>/dev/null
 mkdir -p out/bin
 
-version='0.1.0'
-
-./gradlew makeJar && cp build/libs/java-util-${version}.jar out/bin/java-util-${version}.jar && chmod 770 out/bin/java-util-${version}.jar
+./gradlew makeJar && cp $(ls -tr build/libs/*.jar | tail -1) out/bin/. && chmod 770 out/bin/*.jar
 
