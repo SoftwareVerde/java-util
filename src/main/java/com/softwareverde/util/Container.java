1d236484d4f21d7d5b7cb87a6ed617603c82b28c
Josh Green <josh@softwareverde.com>
2017-09-14 07:10:10 -0400

    Containers now accept default values.

--- src/main/java/com/softwareverde/util/Container.java
+++ src/main/java/com/softwareverde/util/Container.java
@@ -2,4 +2,9 @@ package com.softwareverde.util;
 
 public class Container <T> {
     public T value;
+
+    public Container() { }
+    public Container(final T initialValue) {
+        this.value = initialValue;
+    }
 }
