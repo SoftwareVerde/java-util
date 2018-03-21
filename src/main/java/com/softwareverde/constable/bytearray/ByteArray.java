68ded171de6f9dc04fef44fb0f0a391f229277fd
Josh Green <josh@softwareverde.com>
2018-03-20 21:21:34 -0400

    Added ByteArray to collection.

---
+++ src/main/java/com/softwareverde/constable/bytearray/ByteArray.java
@@ -0,0 +1,16 @@
+package com.softwareverde.constable.bytearray;
+
+import com.softwareverde.constable.Constable;
+
+public interface ByteArray extends Constable<ImmutableByteArray> {
+
+    byte getByte(int index);
+    byte[] getBytes(int index, int byteCount);
+    byte[] getBytes();
+
+    int getByteCount();
+    boolean isEmpty();
+
+    @Override
+    ImmutableByteArray asConst();
+}
