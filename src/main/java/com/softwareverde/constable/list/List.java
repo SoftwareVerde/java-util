ac02dbbcf45c2248f29006a4d2cfa70cdd4ccda4
Josh Green <josh@softwareverde.com>
2019-11-03 12:09:24 -0500

    Deprecated List::getSize and added List::getCount for consistency.

--- src/main/java/com/softwareverde/constable/list/List.java
+++ src/main/java/com/softwareverde/constable/list/List.java
@@ -6,7 +6,11 @@ import com.softwareverde.constable.list.immutable.ImmutableList;
 public interface List<T> extends Iterable<T>, Constable<ImmutableList<T>> {
     T get(int index);
 
+    @Deprecated
     int getSize();
+
+    default int getCount() { return this.getSize(); }
+
     boolean isEmpty();
 
     boolean contains(T item);
