f23bd45a2b71b74b9b26d0fd70b7e33c3d626110
Josh Green <josh@softwareverde.com>
2020-02-01 18:08:27 -0500

    Added MutableByteArray::setBytes.

--- src/main/java/com/softwareverde/constable/bytearray/MutableByteArray.java
+++ src/main/java/com/softwareverde/constable/bytearray/MutableByteArray.java
@@ -28,9 +28,40 @@ public class MutableByteArray implements ByteArray {
         _bytes = new byte[byteCount];
     }
 
+    @Deprecated
     public void set(final int index, final byte b) throws IndexOutOfBoundsException {
+        this.setByte(index, b);
+    }
+
+    public void setByte(final int index, final byte b) throws IndexOutOfBoundsException {
         if (index >= _bytes.length) { throw new IndexOutOfBoundsException(); }
         _bytes[index] = b;
+    }
+
+    /**
+     * Copies the contents of `bytes` into this ByteArray.
+     *  If this ByteArray is smaller than the `startIndex` plus the length of `bytes`, then the excess bytes are ignored and no exception is thrown.
+     *  For example, MutableByteArray(3)::setBytes(1, {1, 2, 3, 4}) results in a ByteArray with the contents: {0, 1, 2}
+     *
+     * @param startIndex
+     *      The index into this ByteArray where `bytes` is written to.
+     *      If the value is outside of the bounds of this ByteArray then an IndexOutOfBoundsException is thrown.
+     * @param bytes
+     *      The content to copy into this ByteArray.
+     */
+    public void setBytes(final int startIndex, final byte[] bytes) throws IndexOutOfBoundsException {
+        if (startIndex >= _bytes.length) { throw new IndexOutOfBoundsException(); }
+
+        ByteUtil.setBytes(_bytes, bytes, startIndex); // Silently aborts before reaching out of bounds.
+    }
+
+    public void setBytes(final int startIndex, final ByteArray bytes) throws IndexOutOfBoundsException {
+        if (startIndex >= _bytes.length) { throw new IndexOutOfBoundsException(); }
+
+        final int byteCopyCount = Math.min(_bytes.length - startIndex, bytes.getByteCount());
+        for (int i = 0; i < byteCopyCount; ++i) {
+            _bytes[startIndex + i] = bytes.getByte(i);
+        }
     }
 
     public void resize(final int newByteCount) {
